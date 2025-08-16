6f3c896bc425c6c4c5e4cd0139048b87e770a5b1
Rohit kumar saw <59217513+Rohit832@users.noreply.github.com>
2025-08-16 07:54:56 +0530

    Commit from TeleportHQ

---
+++ src/views/home.js
@@ -0,0 +1,193 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>Practical Caring Sardine</title>
+        <meta property="og:title" content="Practical Caring Sardine" />
+      </Helmet>
+      <div className="home-group1000003224">
+        <div className="home-group1000003223">
+          <img
+            alt="Rectangle51945"
+            src="/external/rectangle51945-siw.svg"
+            className="home-rectangle5"
+          />
+          <img
+            alt="Ellipse12905195"
+            src="/external/ellipse12905195-pqm9-200h.png"
+            className="home-ellipse1290"
+          />
+          <img
+            alt="Ellipse12915195"
+            src="/external/ellipse12915195-7mof-200h.png"
+            className="home-ellipse1291"
+          />
+          <img
+            alt="Ellipse12925195"
+            src="/external/ellipse12925195-1bgs-200h.png"
+            className="home-ellipse1292"
+          />
+          <img
+            alt="Vector5463"
+            src="/external/vector5463-80j.svg"
+            className="home-vector10"
+          />
+          <img
+            alt="Vector5464"
+            src="/external/vector5464-44t.svg"
+            className="home-vector11"
+          />
+          <img
+            alt="Vector5465"
+            src="/external/vector5465-cper.svg"
+            className="home-vector12"
+          />
+          <img
+            alt="Vector5466"
+            src="/external/vector5466-t1xm.svg"
+            className="home-vector13"
+          />
+          <img
+            alt="Vector5467"
+            src="/external/vector5467-edv.svg"
+            className="home-vector14"
+          />
+          <img
+            alt="Vector5468"
+            src="/external/vector5468-t74k.svg"
+            className="home-vector15"
+          />
+          <div className="home-group1000003221">
+            <img
+              alt="Vector5173"
+              src="/external/vector5173-m02s.svg"
+              className="home-vector16"
+            />
+            <img
+              alt="Vector5174"
+              src="/external/vector5174-82gi.svg"
+              className="home-vector17"
+            />
+          </div>
+          <div className="home-group1000003222">
+            <img
+              alt="Vector5174"
+              src="/external/vector5174-9oz7.svg"
+              className="home-vector18"
+            />
+            <img
+              alt="Vector5174"
+              src="/external/vector5174-13tq.svg"
+              className="home-vector19"
+            />
+          </div>
+        </div>
+        <div className="home-frame1000003265">
+          <div className="home-frame8">
+            <div className="home-frame7">
+              <span className="home-text10">About</span>
+              <span className="home-text11">Contact</span>
+            </div>
+            <img
+              alt="Line21921"
+              src="/external/line21921-jtph.svg"
+              className="home-line2"
+            />
+            <div className="home-frame6">
+              <span className="home-text12">EN</span>
+              <div className="home-mingcutedownline">
+                <div className="home-group">
+                  <img
+                    alt="Vector1976"
+                    src="/external/vector1976-tt9.svg"
+                    className="home-vector20"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <span className="home-text13">Sign In</span>
+          <div className="home-frame1">
+            <span className="home-text14">Book Demo</span>
+          </div>
+        </div>
+        <div className="home-frame1000003316">
+          <div className="home-frame1000003315">
+            <div className="home-frame1000003259">
+              <span className="home-text15">
+                Financial Wellness for Teams That Care
+              </span>
+              <span className="home-text16">
+                Give your employees the tools to stress less, save more, and
+                live better
+              </span>
+            </div>
+            <div className="home-frame1000003260">
+              <div className="home-group1000003217">
+                <img
+                  alt="Ellipse11925"
+                  src="/external/ellipse11925-1m2f-200h.png"
+                  className="home-ellipse1"
+                />
+                <img
+                  alt="Ellipse21925"
+                  src="/external/ellipse21925-07xs-200h.png"
+                  className="home-ellipse2"
+                />
+                <img
+                  alt="Ellipse31925"
+                  src="/external/ellipse31925-od17-200h.png"
+                  className="home-ellipse3"
+                />
+              </div>
+              <div className="home-frame1000003258">
+                <img
+                  alt="mditickcircle5172"
+                  src="/external/mditickcircle5172-9dv.svg"
+                  className="home-mditickcircle"
+                />
+                <span className="home-text17">
+                  <span className="home-text18">1043+</span>
+                  <span className="home-text19">
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                  <span>people already joined</span>
+                </span>
+              </div>
+            </div>
+            <div className="home-frame1000003267">
+              <div className="home-frame2">
+                <span className="home-text21">Book a Demo</span>
+              </div>
+              <div className="home-frame1000003266">
+                <span className="home-text22">See how it works</span>
+                <img
+                  alt="makiarrow5172"
+                  src="/external/makiarrow5172-l70j.svg"
+                  className="home-makiarrow"
+                />
+              </div>
+            </div>
+          </div>
+          <img
+            alt="Rectangle21923"
+            src="/external/rectangle21923-od8o-500w.png"
+            className="home-rectangle2"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
